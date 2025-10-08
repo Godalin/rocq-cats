0e0c1a1477dfaf50132811c252a89c6e9b374ed2
Linyu Yang <yly1228@foxmail.com>
2025-10-08 20:04:48 +0800

    added: natural numbers object

--- theories/ETCS/Nat.v
+++ theories/ETCS/Nat.v
@@ -2,9 +2,14 @@ From Stdlib Require Import Utf8.
 From Cats Require Import Meta.
 From Cats Require Import Cat.Core.
 
-Class HasNNO {U} `(C : Cat U) :=
-  { Nat : Ob
-  ; nat_arr {X} : Hom Nat X
-  ; axiom_natural_object
-    : Type
+Class HasNaturals {U} `(C : Cat U) :=
+  { hasTerminal :: HasTerminal C
+  ; Nat : Ob
+  ; zero : Hom 1 Nat
+  ; succ : Hom Nat Nat
+  ; nat_rec {X} : Hom 1 X → Hom X X → Hom Nat X
+  ; axiom_naturals {X}
+    : ∀ z : Hom 1 X, ∀ s : Hom X X,
+        is_unique (λ f, f ∘ zero = z ∧ f ∘ succ = s ∘ f)
+          (nat_rec z s)
   }.
