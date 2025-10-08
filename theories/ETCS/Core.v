50b8f2dbb02b37e7245da1fa227f1ae1ef568911
Linyu Yang <yly1228@foxmail.com>
2025-10-08 20:10:36 +0800

    updated: ETCS components

--- theories/ETCS/Core.v
+++ theories/ETCS/Core.v
@@ -1,8 +1,21 @@
 From Stdlib Require Import Utf8.
+From Cats Require Import Meta.
 From Cats Require Import Cat.Core.
 From Cats Require Import ETCS.Nat.
 
 Class ETCS U :=
   { C :: Cat U
-
+  ; hasTerminal :: HasTerminal C
+  ; hasProduct :: HasProduct C
+  ; hasExponential :: HasExponential hasProduct
+  ; hasNaturals :: HasNaturals C
   }.
+
+
+
+Section ETCS.
+Context {U} `{ETCS U}.
+
+Check π1 ∘ (id × ƛ id).
+
+End ETCS.
